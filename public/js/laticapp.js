6db1cf08893a32026ad037a548587ce89b1faf13
Tom Pennington <tompennington.92@gmail.com>
2016-02-21 20:20:11 +0000

    Styles and icons.

--- public/js/laticapp.js
+++ public/js/laticapp.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
     renderHandlebarsTemplate('templates/tweets.handlebars', '#results', data);
   });
 
+  Handlebars.registerHelper('noop', function(options) {
+    var text = this.text;
+    var t2 = text.replace(/(http|https)(:\/\/)([^ ]+)/ig, '<a href="$1$2$3">$1$2$3</a>' );
+    t2 = t2.replace(/@([^ :]+)/gi,'<a href="http://twitter.com/$1" target="_blank">@$1</a>');
+    t2 = t2.replace(/#([^ :]+)/gi,'<a href="http://twitter.com/search?q=%23$1" target="_blank">#$1</a>');
+    return t2;
+  });
+
+  Handlebars.registerHelper('formatDate', function(date) {
+    return moment(date).startOf('hour').fromNow();
+  });
+
   function renderHandlebarsTemplate(withTemplate, inElement, withData){
     getTemplateAjax(withTemplate, function(template) {
       $(inElement).html(template(withData));
