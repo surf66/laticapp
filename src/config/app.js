afe5b1d9d3e163ad2c385ec5d67f9d61c2cab202
Tom Pennington <tompennington.92@gmail.com>
2016-02-21 17:29:41 +0000

    Moved everything inside src folder.

---
+++ src/config/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var social = require("../functions/social.js");
+
+module.exports = {
+  setup: function() {
+    app.use(express.static('../public'));
+    app.set('view engine', 'html');
+    app.set('layout', 'layout');
+    app.set('partials', {
+      head: 'head.html',
+      header: 'header.html',
+      footer: 'footer.html'
+    });
+    app.engine('html', require('hogan-express'));
+
+    app.get('/', function(req, res){
+      res.render('index.html');
+    });
+
+    app.get('/tweets', function(req, res) {
+      social.getTweets().then(function(response) {
+        res.send(response);
+      });
+    });
+  },
+  startServer: function() {
+    var server = app.listen(3000, function(){
+      var host = server.address().address;
+      var port = server.address().port;
+      console.log("Server up @ http://%s:%s", host, port);
+    });
+  }
+}
